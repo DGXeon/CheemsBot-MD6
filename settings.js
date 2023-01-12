5af7ad7c9153ecf7528d355272925c4643af74c6
Ayush <117559953+Ayushdeveloper001@users.noreply.github.com>
2023-01-12 14:28:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -35,7 +35,7 @@ global.wm = "Ayush Bot Inc." //ur watermark
 
 // Other
 global.sessionName = 'session'
-global.prefa = ['&']
+global.prefa = ['&''','!','.','🐦','🐤','🗿']
 global.sp = ''
 
 //messages
